bcabcd03541fbc65149696966db06e5290fb3abb
Tommi Hovi <tommi.hovi@gmail.com>
2022-10-07 00:19:56 +0300

    Improve headers logging

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
+import json
 from typing import Union
 
 from fastapi import FastAPI, Request
+from starlette.datastructures import Headers
 
 app = FastAPI()
 
 
+def sorted_headers(headers: Headers) -> dict:
+    items = headers.items()
+    # Sort based on the first item (which is the key)
+    items.sort(key=lambda x: x[0])
+    # Convert the format to make it look better in jq
+    items = [x[0] + " = " + x[1] for x in items]
+
+    return json.dumps({"headers": items})
+
+
 @app.get("/")
 def read_root(request: Request):
-    print(request.headers)
+    # Log headers
+    print(sorted_headers(request.headers))
+
     return {"Hello": "World"}
 
 
